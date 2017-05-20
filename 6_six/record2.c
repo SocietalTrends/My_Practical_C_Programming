050a002f01888e3d969b8033407b57a75a4237ae
Societaltrends <app.scieppan@gmail.com>
2017-05-20 23:47:45 +0900

    Practice C

--- 6_six/record2.c
+++ 6_six/record2.c
@@ -6,12 +6,14 @@
 char value[100];
 int percent; /* 正解% */
 char record[2];
-char min[1];	//マイナス記号
-char plu[1];	//プラス記号
+char *min[1];	//マイナス記号
+char *plu[1];	//プラス記号
 
 int main(){
-		strcpy(min,'-');
-		strcpy(plu,'+');
+		/* strcpy関数の仕様をもう一度確認すること */
+		/* 特にポインタ関係 */
+		*min = "-";
+		*plu = "+";
 		printf("Enter your score.\n");
 		fgets(value, sizeof(value), stdin);
 		sscanf(value, "%d", &percent);
@@ -20,18 +22,39 @@ int main(){
 		}else if(percent <= 70){
 				strcpy(record, "D");
 				if(percent <= 63){
-						strcat(record,min);
+						strcat(record,*min);
 				}else if(percent <= 67){
 
 				}else if(percent <= 70){
-						strcat(record,plu);
+						strcat(record,*plu);
 				}		
 		}else if(percent <= 80){
 				strcpy(record, "C");
+				if(percent <= 73){
+						strcat(record,*min);
+				}else if(percent <= 77){
+
+				}else if(percent <= 80){
+						strcat(record,*plu);
+				}
 		}else if(percent <= 90){
 				strcpy(record, "B");
+				if(percent <= 83){
+						strcat(record,*min);
+				}else if(percent <= 87){
+
+				}else if(percent <= 90){
+						strcat(record,*plu);
+				}
 		}else if(percent <= 100){
 				strcpy(record, "A");
+				if(percent <= 93){
+						strcat(record,*min);
+				}else if(percent <= 97){
+
+				}else if(percent <= 100){
+						strcat(record,*plu);
+				}
 		}else{
 				printf("Please enter your correct score.\n");
 				return(0);
