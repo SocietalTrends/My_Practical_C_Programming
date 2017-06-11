02f2d13d7675e7c02dd858bace3573ed2fb3ddab
Societaltrends <app.scieppan@gmail.com>
2017-06-12 01:53:58 +0900

    Practice C

--- kara/kara4.c
+++ kara/kara4.c
@@ -2,9 +2,9 @@
 #include<stdio.h>
 #include<stdlib.h>
 int main(){
-		char s[6][3] = {{"唐"},{"澤"},{"貴"},{"洋"},{"食"},{"す"}};
+		char s[7][4] = {{"唐"},{"澤"},{"貴"},{"洋"},{"食"},{"す"}};
 		for(int i=0;i<6;i++){
-				//printf("s[%d]:%s\n", i, s[i]);
+				// printf("s[%d]:%s\n", i, s[i]);
 				printf("%s", s[rand()%6]);
 		}
 }
